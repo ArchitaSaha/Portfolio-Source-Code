5ec8d987b08faf8e8f73e59ca014546c5f717399
Archita Saha <sahaarchita9@gmail.com>
2022-12-13 02:14:53 +0530

    Version 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ProjectsComponent } from './projects/projects.component';
 import { SkillsetComponent } from './skillset/skillset.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { WorkComponent } from './work/work.component';
+import { ResearchComponent } from './research/research.component';
 // import { ProjectListComponent } from './project-list/project-list.component';
 
 @NgModule({
@@ -25,6 +26,7 @@ import { WorkComponent } from './work/work.component';
     SkillsetComponent,
     ContactsComponent,
     WorkComponent,
+    ResearchComponent,
     // ProjectListComponent
   ],
   imports: [
